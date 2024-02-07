819bee5c6ef9ee03f530286a21d5085b9939de78
Ricardo Polanski <ricardo.polanski@ericsson.com>
2024-02-07 12:04:06 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SetupEnvironment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            
+
+            driver.Navigate().GoToUrl("http://testing.todorvachev.com/selectors/name/");
+
+            IWebDriver element = (IWebDriver)driver.FindElement(By.Name(""));
+
+            Thread.Sleep(5000);
+
+            driver.Quit();
+
+        }
+    }
+}
